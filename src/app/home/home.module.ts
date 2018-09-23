e230432060175effdbdf3c3f108b5a0f614a6803
Rodolphe Vanderaspoilden <rodolphevanderaspoilden@gmail.com>
2018-09-23 17:57:03 +0200

    :sparkles: add carousel + crop image

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomeComponent} from './home.component';
 import {RouterModule} from '@angular/router';
 import {ROUTES} from './home.route';
+import {CarouselModule} from 'ngx-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(ROUTES)
+    RouterModule.forChild(ROUTES),
+    CarouselModule.forRoot(),
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent],
+  bootstrap: [HomeComponent]
 })
 export class HomeModule { }
